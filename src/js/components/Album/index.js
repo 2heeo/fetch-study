5b08bdcebae00306d18766b6a846f71839efb9da
2heeo <2hee.o.walkr@gmail.com>
2021-03-14 00:43:00 +0900

    async-await 적용

--- src/js/components/Album/index.js
+++ src/js/components/Album/index.js
@@ -6,11 +6,19 @@ class Album {
     this.appElement = document.querySelector(appSelector);
   }
 
-  init() {
+  // async-await 이 사용되는 함수 fetchAlbumFiles를 쓰려면
+  // 사용하는 곳에서도 async-await를 써야한다.
+  // 안그러면 데이터가 아니라 Promise가 반환됨
+  async init() {
     this.render();
 
-    // 함수 호출
-    // console.log(responseBody);
+    const responseBody = await window.api.fetchAlbumFiles();
+    console.log(responseBody);
+
+    // window.api.fetchAlbumFiles()
+    // .then((responseBody) => {
+    //   console.log(responseBody);
+    // });
   }
 
   render() {
