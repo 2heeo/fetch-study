76ac7f1943f6ac9bdff9611ab51fc54001be394c
2heeo <2hee.o.walkr@gmail.com>
2021-03-18 22:10:43 +0900

    parent 값 사용해서 작업중..

--- src/js/components/Album/index.js
+++ src/js/components/Album/index.js
@@ -42,33 +42,50 @@ class Album {
     this.clickDirectory(responseBody);
   }
 
+  isParentsNull(responseBody) {
+    const parentItems = responseBody.filter(item => item.parent === null);
+    
+    console.log(`부모가 없냐? ${parentItems.length !== 0}`);
+    return (parentItems.length !== 0);
+  }
+
   reset() {
     this.appElement.innerHtml = '';
   }
 
   clickDirectory(responseBody) {
     const nodes = this.appElement.querySelectorAll('.node');
+    let requestURL = this.requestURL; // todo-heeo. 변수명 고치기
     let clickedDirId = '';
 
     this.appElement.addEventListener('click', (event) => {      
       const path = event.path;
-      const isRootDirectory = responseBody[0].parent === null;  
 
-      path.some(el => {
-        console.log(el);
-        if(typeof el !== undefined && el?.classList.contains('node')) {
-          const clickedDirIndex = isRootDirectory ? Array.from(nodes).indexOf(el) : Array.from(nodes).indexOf(el) + 1;
-
-          clickedDirId = `/${responseBody[clickedDirIndex].id}`;
-          console.log(`id = ${clickedDirId}`);
-          return true;
+      for(let i = 0; i < path.length; i ++) {
+        // 뒤로가기 버튼 클릭
+        if(path[i].classList.contains('btn_back')){ // 뒤로가기 버튼 클릭한 경우
+          requestURL = this.isParentsNull(responseBody) ? requestURL : `${this.requestURL}/${path[i].parent}`; // parent가 null 이면 이전 url 유지, 아니면 /${새로운 id}
+        
+          console.log(33, this.isParentsNull(responseBody));
+          this.isParentsNull(responseBody) ? console.log(1, requestURL) : console.log(2, requestURL);
+          break;
         }
-      });
+        // 디렉토리/파일 클릭
+        else if(path[i].classList.contains('node')) { // 디렉토리나 파일 클릭시
+          // 상위폴더 없으면(ROOT) 뒤로가기버튼 없으니까 클릭한 노드 인덱스와 동일한 api 데이터 선택, 아니면 뒤로가기버튼 인덱스 포함해서 +1
+          const clickedDirIndex = this.isParentsNull(responseBody) ? Array.from(nodes).indexOf(path[i]) : Array.from(nodes).indexOf(path[i]) + 1;
 
-      this.requestURL = this.requestURL + clickedDirId;
-      
+          clickedDirId = `${responseBody[clickedDirIndex].id}`; // 클릭한 노드의 id
+          requestURL = `${this.requestURL}/${clickedDirId}`;
+
+          console.log(`id = ${clickedDirId}`);
+          console.log(responseBody[clickedDirIndex]);
+          console.log(requestURL);
+          break;
+        } 
+      }      
       this.reset();
-      this.render(this.requestURL);
+      this.render(requestURL);
     });
   }
 }
